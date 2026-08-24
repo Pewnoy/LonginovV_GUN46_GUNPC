1344fc6a195c9a215f14b743e145c3ec364b94ff
Pew <memeris2@gmail.com>
2026-08-24 12:08:29 +0300

    Решение по теме Циклы
    Версия кода после работы над предупреждениями

---
+++ Homework2/Homework3/Homework3/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace HomeWork3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Задание 1
+
+            int a = 0;
+            int b = 1;
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(a);
+
+                int next = a + b;
+                a = b;
+                b = next;
+            }
+
+
+            // Задание 2
+
+            for (int i = 2; i <= 20; i += 2)
+            {
+                Console.WriteLine(i);
+            }
+
+
+            // Задание 3
+
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 1; j <= 5; j++)
+                {
+                    Console.Write(i * j + " ");
+                }
+
+                Console.WriteLine();
+            }
+
+
+            // Задание 4
+
+            string password = "qwerty";
+            string? input;
+
+            do
+            {
+                Console.WriteLine("Enter password:");
+                input = Console.ReadLine();
+            }
+            while (input != password);
+        }
+    }
+}
