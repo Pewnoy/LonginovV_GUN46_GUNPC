b4f80d092f6241ebcddae328943c0e3a014c1955
Pew <memeris2@gmail.com>
2026-08-27 12:14:33 +0300

    Решение работы по классам
    Решение работы по классам - первый вариант

--- Homework4/Program.cs
+++ Homework4/Program.cs
@@ -1,1 +1,32 @@
-﻿
+﻿class Unit
+{
+    private float health;
+    public string Name { get; }
+    public float Health
+    {
+        get { return health; }
+    }
+
+    public int Damage { get; }
+    public float Armor { get; }
+    public Unit(string name)
+    {
+        Name = name;
+        health = 100f;
+        Damage = 5;
+        Armor = 0.6f;
+    }
+
+    public Unit() : this("Unknown Unit")
+    public float GetRealHealth()
+    {
+        return Health * (1f + Armor);
+    }
+
+    public bool SetDamage(float value)
+    {
+        health = Health - value * Armor;
+
+        return Health <= 0f;
+    }
+}
