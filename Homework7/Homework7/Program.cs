1b426f5007438e46c667c2994873dd0a43988de3
Pew <memeris2@gmail.com>
2026-09-09 12:08:19 +0300

    Доработка задания
    -Добавлен Main
    -Добавлены вызовы всех шести методов
    -Задание 4 — Substring
    -Задание 6 — Replace

--- Homework7/Homework7/Program.cs
+++ Homework7/Homework7/Program.cs
@@ -27,7 +27,7 @@ namespace HomeWork7
         // Задание 4
         public static string GetFirstFiveCharacters(string input)
         {
-            return input.Substring(0, 5);
+            return input.Substring(0, Math.Min(5, input.Length));
         }
 
         // Задание 5
@@ -50,11 +50,64 @@ namespace HomeWork7
 
         // Задание 6
         public static string ReplaceWords(
-            string inputString,
-            string wordToReplace,
-            string replacementWord)
+        string inputString,
+        string wordToReplace,
+        string replacementWord)
         {
+            if (string.IsNullOrEmpty(inputString) ||
+                string.IsNullOrEmpty(wordToReplace) ||
+                string.IsNullOrEmpty(replacementWord))
+            {
+                return inputString;
+            }
+
             return inputString.Replace(wordToReplace, replacementWord);
+        }
+        static void Main(string[] args)
+        {
+            // Проверка задания 1
+            string result1 = ConcatenateStrings("Hello ", "world!");
+            Console.WriteLine(result1);
+
+            Console.WriteLine();
+
+
+            // Проверка задания 2
+            string result2 = GreetUser("Victor", 30);
+            Console.WriteLine(result2);
+
+            Console.WriteLine();
+
+
+            // Проверка задания 3
+            string result3 = GetStringInfo("Hello World");
+            Console.WriteLine(result3);
+
+            Console.WriteLine();
+
+
+            // Проверка задания 4
+            string result4 = GetFirstFiveCharacters("Hello world");
+            Console.WriteLine(result4);
+
+            Console.WriteLine();
+
+
+            // Проверка задания 5
+            string[] words = { "CSharp", "is", "interesting" };
+            StringBuilder result5 = BuildSentence(words);
+            Console.WriteLine(result5);
+
+            Console.WriteLine();
+
+
+            // Проверка задания 6
+            string result6 = ReplaceWords(
+                "Hello world",
+                "world",
+                "universe");
+
+            Console.WriteLine(result6);
         }
     }
 }
